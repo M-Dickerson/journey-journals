ae61ee1d48d8e37e37bdcba317f3735438f1c895
Ryan Belcher <ryanmbelcher86@gmail.com>
2023-01-31 13:54:07 -0500

    added profile info section

--- client/src/components/pages/ProfilePage.js
+++ client/src/components/pages/ProfilePage.js
@@ -2,3 +2,22 @@ import React from "react"
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+
+
+export default function ProfilePage() {
+    return (
+        <Container>
+            <Row>
+                <span>Profile Info</span>
+                <a>Edit</a>
+                {/* profile picure: */}
+                <img></img>
+                <span>Jane Doe</span>
+                <span>Bio: I love nature</span>
+                <span>Posts: </span>
+                <span>Followers: </span>
+                <button>Follow Me</button>
+            </Row>
+        </Container>
+    )
+}
