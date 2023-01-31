b22c378a59e16c0d1318357472c94d61c8becef1
Ryan Belcher <ryanmbelcher86@gmail.com>
2023-01-31 12:08:55 -0500

    imported react bootstrap

--- client/src/components/pages/TravelFeed.js
+++ client/src/components/pages/TravelFeed.js
@@ -1,24 +1,34 @@
 import React from 'react'
-
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 
 export default function TravelFeed() {
     return (
-        <div>
+        <Container>
             <h2>Travel Feed</h2>
-            <div>
-                <img>Profile Image</img>
-                <span>title</span>
-                <h3>Day 1</h3>
-                <span>date</span>
-                <img>Trip Images Here</img>
-                <span>Description: </span>
-                <p>***description text***</p>
-                <span>Comment/Like</span>
-            </div>
-            <div>
+            <Row>
+                <Col>
+                    <img>Profile Image</img>
+                    <span>title</span>
+                    <span>Description: </span>
+                    <p>***description text***</p>
+                </Col>
+                <Col>
+                    <h3>Day 1</h3>
+                    {/* do we want to use glide.js or bootstrap to carousel images? */}
+                    <img>Trip Images Here</img>
+                </Col>
+                <Col>
+                    <span>date</span>
+                    {/* how are we handling comments and likes? */}
+                    <span>Comment/Like</span>
+                </Col>
+            </Row>
+            <Row>
                 {/* 3rd party API--recommendations, etc */}
-            </div>
-        </div>
+            </Row>
+        </Container>
     );
 }
