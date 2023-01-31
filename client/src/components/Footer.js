0a3858f4f8f21f83d906632834fb93e3d4ca8edf
D <Dickerson.Morgan@outlook.com>
2023-01-31 13:50:47 -0500

    added and styled a footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -4,7 +4,7 @@ import "../styles/Footer.css";
 export default function Footer() {
     return (
         <div className="footer">
-            <p> :; </p>
+            <p>Made with ♡ by Ryan B, Morgan D, and Avani M</p>
         </div>
     );
 }
