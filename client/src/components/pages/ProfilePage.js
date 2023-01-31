16fa9c168f8bec0d887ba038d0b449db12ff4a5b
D <Dickerson.Morgan@outlook.com>
2023-01-31 17:33:01 -0500

    wrapped ProfilePage in a card for better visibility

--- client/src/components/pages/ProfilePage.js
+++ client/src/components/pages/ProfilePage.js
@@ -1,30 +1,35 @@
 import React from "react"
+// links for react bootstrap styling
+import "../../styles/ProfilePage.css";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
+import Card from 'react-bootstrap/Card';
 // import Col from "react-bootstrap/Col";
 
 
 export default function ProfilePage() {
     return (
         <Container>
-            <Row>
-                <p>Profile Info</p>
-                <p>Edit</p>
-                {/* profile picure: */}
-                <img alt="placeholder"></img>
-                <p>Jane Doe</p>
-                <p>Bio: I love nature</p>
-                <p>Posts: </p>
-                <p>Followers: </p>
-                <button>Follow Me</button>
-            </Row>
-            <Row>
-                <p>Saved places/places I want to visit:</p>
-            </Row>
-            <Row>
-                <p>Profile Info</p>
-                <p>Edit</p>
-            </Row>
+            <Card>
+                <Row>
+                    <p>Profile Info</p>
+                    <p>Edit</p>
+                    {/* profile picure: */}
+                    <img alt="placeholder"></img>
+                    <p>Jane Doe</p>
+                    <p>Bio: I love nature</p>
+                    <p>Posts: </p>
+                    <p>Followers: </p>
+                    <button>Follow Me</button>
+                </Row>
+                <Row>
+                    <p>Saved places/places I want to visit:</p>
+                </Row>
+                <Row>
+                    <p>Profile Info</p>
+                    <p>Edit</p>
+                </Row>
+            </Card>
         </Container>
     );
 }
