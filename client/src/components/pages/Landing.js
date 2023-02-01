6662244d50449d1500c8f8c71026e1e64bd7f89e
D <Dickerson.Morgan@outlook.com>
2023-02-01 12:05:31 -0500

    mobile responsiveness for landing page is done

--- client/src/components/pages/Landing.js
+++ client/src/components/pages/Landing.js
@@ -10,25 +10,26 @@ export default function Landing() {
     return (
         <Container>
             <Row className="landing">
-                <Col xl={4} sm={8} xs={9} className="landing1">
-                    <p className="landingT">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                <Col xl={5} sm={4} xs={9} className="landing1">
+                    <p className="landingT">Hi there! Welcome to your destination! </p>
                 </Col>
-                <Col xl={6} sm={8} xs={9} className="landing2">
-                    <Card style={{ width: '18rem' }}>
+                <Col xl={3} sm={5} xs={7} className="landing2">
+                    <Card style={{ width: '25vh' }}>
                         <Card.Img variant="top" src="https://www.marineinsight.com/wp-content/uploads/2019/08/Cruise-ships-1.png" />
                         <Card.Body>
                             <Card.Title>In the mood to cruise?</Card.Title>
                             <Card.Text>
-                                Find your next cruise here!
+                                Find your next one here!
                             </Card.Text>
                         </Card.Body>
                     </Card>
-                    <Card style={{ width: '18rem' }}>
-                        <Card.Img variant="top" src="https://www.marineinsight.com/wp-content/uploads/2019/08/Cruise-ships-1.png" />
+                    <br></br>
+                    <Card style={{ width: '25vh' }}>
+                        <Card.Img variant="top" src="https://wallpaperaccess.com/full/2152627.jpg" />
                         <Card.Body>
-                            <Card.Title>In the mood to cruise?</Card.Title>
+                            <Card.Title>Can't take the heat?</Card.Title>
                             <Card.Text>
-                                Find your next cruise here!
+                                Find a nice cabin to chill in here!
                             </Card.Text>
                         </Card.Body>
                     </Card>
