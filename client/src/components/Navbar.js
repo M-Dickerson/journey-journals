89ad4f635159d0d4b037d2ce3f243c0850b796fb
D <Dickerson.Morgan@outlook.com>
2023-02-04 21:29:33 -0500

    got the background to go down all the way. added hamburger menu

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,28 +1,31 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 // links for react bootstrap styling
 import "../styles/Navbar.css";
 import { Navbar, Nav, Container, Modal, Tab } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import SignupForm from './SignupForm';
 import LoginForm from './LoginForm';
+import { DarkModeContext } from '../context/DarkModeContext';
 
 import Auth from '../utils/auth';
 
 function AppNavbar() {
     // Set modal display state
     const [showModal, setShowModal] = useState(false);
+    const { toggleDarkMode, darkMode } = useContext(DarkModeContext)
+    console.log(darkMode)
 
     return (
         <>
-            <Navbar>
+            <Navbar collapseOnSelect expand="xl" variant="dark">
                 <Container className="navCon">
                     {!Auth.loggedIn() ? (
                         <Navbar.Brand className="jj">Journey Journals</Navbar.Brand>
                     ) : (
-                        <Navbar.Brand className="jj">{Auth.getProfile().data.username}'s Journey Journals</Navbar.Brand>
+                        <Navbar.Brand className="jj darkButton" onClick={toggleDarkMode}>{Auth.getProfile().data.username}'s Journey Journals</Navbar.Brand>
                     )}
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                    <Navbar.Collapse id="basic-navbar-nav">
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
                         <Nav className="me-auto">
                             {/* If logged in, render all accessible tabs in navbar */}
                             {Auth.loggedIn() ? (
@@ -38,10 +41,6 @@ function AppNavbar() {
                                     <Link className=" navLinks" to="/logout" onClick={Auth.logout}>
                                         Logout
                                     </Link>
-
-                                    {/* <Nav.Link href="#TravelFeed" onClick={() => handlePageChange("TravelFeed")}>TravelFeed</Nav.Link>
-                                    <Nav.Link href="#ProfilePage" onClick={() => handlePageChange("ProfilePage")}>ProfilePage</Nav.Link>
-                                    <Nav.Link onClick={Auth.logout}>Logout</Nav.Link> */}
                                 </>
                             ) : (
                                 <Link className=" navLinks" to="/" onClick={() => setShowModal(true)}>Login</Link>
@@ -91,35 +90,3 @@ function AppNavbar() {
 }
 
 export default AppNavbar;
-
-
-// <Modal
-//     size='lg'
-//     show={showModal}
-//     onHide={() => setShowModal(false)}
-//     aria-labelledby='signup-modal'
-//     centered>
-
-//     <Tab.Container defaultActiveKey='login'>
-//         <Modal.Header closeButton>
-//             <Modal.Title id='login-modal'>
-//                 <Nav variant='pills'>
-//                     <Nav.Item>
-//                         <Nav.Link className="something2" eventKey='login'>Login</Nav.Link>
-//                     </Nav.Item>
-//                     <Nav.Item>
-//                         <Nav.Link className="something2" eventKey='signup'>Sign Up</Nav.Link>
-//                     </Nav.Item>
-//                 </Nav>
-//             </Modal.Title>
-//         </Modal.Header>
-
-//         <Modal.Body>
-//             <Tab.Content>
-//                 <Tab.Pane eventKey='login'>
-//                     <Contact handleModalClose={() => setShowModal(false)} />
-//                 </Tab.Pane>
-//             </Tab.Content>
-//         </Modal.Body>
-//     </Tab.Container>
-// </Modal>
